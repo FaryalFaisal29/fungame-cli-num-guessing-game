b7ef1e890b900f8136011d673eba85d37c6a8e4b
FaryalFaisal29 <164797311+FaryalFaisal29@users.noreply.github.com>
2024-03-30 01:41:24 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,24 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const randomNumber = Math.floor(Math.random()*10+1);
+
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Please guess a number between 1-10:",
+
+},
+]);
+
+let userGuessedNumber = randomNumber;
+
+switch(answers){
+    case "randomNumber":
+        console.log("Congratulations! You have guessed right number");
+        break;
+        default:
+            console.log("Sorry! you are lost, try again");
+}
